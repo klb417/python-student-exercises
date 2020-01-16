9669406d1e72af709168701f73dda3c6eca0c3c2
Ken Boyd <klb417@gmail.com>
2020-01-16 12:59:42 -0600

    finished part one of student exercises

---
+++ cohort.py
@@ -0,0 +1,17 @@
+class Cohort:
+    def __init__(self, name):
+        self.name = name
+        self.students = list()
+        self.instructors = list()
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def add_instructor(self, instructor):
+        self.instructors.append(instructor)
+
+    def add_students(self, students):
+        self.students.extend(students)
+
+    def add_instructors(self, instructors):
+        self.instructors.extend(instructors)
